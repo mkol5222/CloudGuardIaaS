641abcb63ac11d0a28dabc510530da13e47f57a2
Martin Koldodovsky <mkoldov@gmail.com>
2024-02-28 07:19:18 +0100

    fx

--- terraform/azure/high-availability-existing-vnet/main.tf
+++ terraform/azure/high-availability-existing-vnet/main.tf
@@ -179,7 +179,7 @@ resource "azurerm_network_interface_backend_address_pool_association" "nic1_lb_a
   count = 2
   network_interface_id    = azurerm_network_interface.nic1[count.index].id
   ip_configuration_name   = "ipconfig2"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.backend-lb-pool[0].id
+  backend_address_pool_id = azurerm_lb_backend_address_pool.backend-lb-pool.id
 }
 
 //********************** Load Balancers **************************//
@@ -265,7 +265,7 @@ resource "azurerm_lb_rule" "backend_lb_rules" {
   backend_port = 0
   frontend_ip_configuration_name = "backend-lb"
   load_distribution = "Default"
-  backend_address_pool_id = azurerm_lb_backend_address_pool.backend-lb-pool[0].id
+  backend_address_pool_id = azurerm_lb_backend_address_pool.backend-lb-pool.id
   probe_id = azurerm_lb_probe.azure_lb_healprob_back.id
   enable_floating_ip = var.enable_floating_ip
 }
